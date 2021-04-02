2dc0c6686f440ff27c83cd41544ffbb889072541
Eber Peña <eberg.pena@gmail.com>
2021-04-01 19:10:59 -0600

    Se ha generado una prueba con Ganache

--- compile-and-deploy/scripts/compile.js
+++ compile-and-deploy/scripts/compile.js
@@ -6,8 +6,4 @@ const chalk = require('chalk');
 const contractPath = path.resolve(__dirname, "../contracts", "UsersContract.sol");
 const source = fs.readFileSync(contractPath, 'utf8');
 
-
 module.exports = solc.compile(source, 1).contracts[':UsersContract'];
-
-
-
